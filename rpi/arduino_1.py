a63738519b41ecc79525db168c5a5abdda80bae7
DedRobs <41262949+DedRobs@users.noreply.github.com>
2020-09-02 13:52:23 +0800

    Update and rename arduino.py to arduino_1.py

--- rpi/arduino_1.py
+++ rpi/arduino_1.py
@@ -1,7 +1,8 @@
 import serial
 
 # receiving input from the Arduino, "Hello World!"
-ser = serial.Serial('/dev/ttyACM0', 9600)
+# ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_75833353035351603131-if00', 9600)
 
 # Transmitting output to Arduino, 'S'
 testArduino = 'S'
