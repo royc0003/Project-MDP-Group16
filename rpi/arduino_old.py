479714bc12228ed56fd055ec47299e306179ae78
DedRobs <41262949+DedRobs@users.noreply.github.com>
2020-09-02 21:23:53 +0800

    Test #1

---
+++ rpi/arduino_old.py
@@ -0,0 +1,17 @@
+import serial
+
+# receiving input from the Arduino, "Hello World!"
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+# Transmitting output to Arduino, 'S'
+testArduino = 'S'
+# Converting unicode to bytes; can also be converted to a different type
+encodeArduino = testArduino.encode()
+
+while True:
+    if(ser.in_waiting > 0):
+        # prints the string from the Arduino, "Hello World!"
+        line = ser.readline()
+        print(line)
+        # sending a test string to the Arduino from the Rpi
+        ser.write(encodeArduino)
