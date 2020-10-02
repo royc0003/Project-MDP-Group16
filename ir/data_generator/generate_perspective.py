e620cd5c2cd3a5654b20202b5acdcae7d00273ec
ongcj <chengjie13@hotmail.com>
2020-10-02 22:34:03 +0800

    wrap up image perspective transformation

--- ir/data_generator/generate_perspective.py
+++ ir/data_generator/generate_perspective.py
@@ -1,22 +1,26 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from os.path import join
+from PIL import Image
+from matplotlib import cm
 
 # pick one upright image
 PATH_TO_IMAGE = "/Users/ongcj/Dev/mdp/ir/data/processed/nine_resized/IMG_0262_RESIZED_RANDOM_RESIZE_RESIZED.JPG"
+PATH_TO_TARGET_DIRECTORY = "/Users/ongcj/Dev/mdp/ir/data/training/nine/p"
 
 
 def left_perspective_coordinates(img, percent):
     y, x, ch = img.shape
     # calculate new coordinates
     # reduce left by 20%, need to reduce y
-    beta = y/100 * percent
+    beta = y / 100 * percent
 
     current_coordinates = np.float32([[0, 0], [x, 0], [0, y], [x, y]])
     new_coordinates = np.float32([[0, 0],
-                                  [x, beta], # change
+                                  [x, beta],  # change
                                   [0, y],
-                                  [x, y - beta]] # change
+                                  [x, y - beta]]  # change
                                  )
 
     return current_coordinates, new_coordinates, (x, y)
@@ -26,12 +30,12 @@ def right_perspective_coordinates(img, percent):
     y, x, ch = img.shape
     # calculate new coordinates
     # reduce left by 20%, need to reduce y
-    beta = y/100 * percent
+    beta = y / 100 * percent
 
     current_coordinates = np.float32([[0, 0], [x, 0], [0, y], [x, y]])
-    new_coordinates = np.float32([[0, beta], # change
+    new_coordinates = np.float32([[0, beta],  # change
                                   [x, 0],
-                                  [0, y - beta], # change
+                                  [0, y - beta],  # change
                                   [x, y]]
                                  )
 
@@ -42,13 +46,13 @@ def top_perspective_coordinates(img, percent):
     y, x, ch = img.shape
     # calculate new coordinates
     # reduce left by 20%, need to reduce y
-    alpha = x/100 * percent
+    alpha = x / 100 * percent
 
     current_coordinates = np.float32([[0, 0], [x, 0], [0, y], [x, y]])
     new_coordinates = np.float32([[0, 0],
                                   [x, 0],
-                                  [alpha, y], # change
-                                  [x - alpha, y]] # change
+                                  [alpha, y],  # change
+                                  [x - alpha, y]]  # change
                                  )
 
     return current_coordinates, new_coordinates, (x, y)
@@ -58,11 +62,11 @@ def bottom_perspective_coordinates(img, percent):
     y, x, ch = img.shape
     # calculate new coordinates
     # reduce left by 20%, need to reduce y
-    alpha = x/100 * percent
+    alpha = x / 100 * percent
 
     current_coordinates = np.float32([[0, 0], [x, 0], [0, y], [x, y]])
-    new_coordinates = np.float32([[alpha, 0], # change
-                                  [x - alpha, 0], # change
+    new_coordinates = np.float32([[alpha, 0],  # change
+                                  [x - alpha, 0],  # change
                                   [0, y],
                                   [x, y]]
                                  )
@@ -70,34 +74,38 @@ def bottom_perspective_coordinates(img, percent):
     return current_coordinates, new_coordinates, (x, y)
 
 
-if __name__ == "__main__":
-    img = cv2.imread(PATH_TO_IMAGE)
+def convert_to_pil(img_ndarray):
+    return Image.fromarray(np.uint8(cm.gist_earth(img_ndarray) * 255))
 
-    # since image are normally more *longer*, we choose a percent that is larger for left and right
+
+def write_to_output(img):
     for percent in range(3, 15, 3):
+
         pts1, pts2, end_size = left_perspective_coordinates(img, percent)
         M = cv2.getPerspectiveTransform(pts1, pts2)
         dst = cv2.warpPerspective(img, M, end_size)
-        plt.subplot(121), plt.imshow(img), plt.title('Input')
-        plt.subplot(122), plt.imshow(dst), plt.title('Output')
-        plt.show()
+        cv2.imwrite(join(PATH_TO_TARGET_DIRECTORY,
+                         PATH_TO_IMAGE.split("/")[-1].replace(".JPG", "_LEFT_" + str(percent) + ".JPG")), dst)
+
         pts1, pts2, end_size = right_perspective_coordinates(img, percent)
         M = cv2.getPerspectiveTransform(pts1, pts2)
         dst = cv2.warpPerspective(img, M, end_size)
-        plt.subplot(121), plt.imshow(img), plt.title('Input')
-        plt.subplot(122), plt.imshow(dst), plt.title('Output')
-        plt.show()
+        cv2.imwrite(join(PATH_TO_TARGET_DIRECTORY,
+                         PATH_TO_IMAGE.split("/")[-1].replace(".JPG", "_RIGHT_" + str(percent) + ".JPG")), dst)
 
-    for percent in range(3, 15, 3):
         pts1, pts2, end_size = top_perspective_coordinates(img, percent)
         M = cv2.getPerspectiveTransform(pts1, pts2)
         dst = cv2.warpPerspective(img, M, end_size)
-        plt.subplot(121), plt.imshow(img), plt.title('Input')
-        plt.subplot(122), plt.imshow(dst), plt.title('Output')
-        plt.show()
+        cv2.imwrite(join(PATH_TO_TARGET_DIRECTORY,
+                         PATH_TO_IMAGE.split("/")[-1].replace(".JPG", "_TOP_" + str(percent) + ".JPG")), dst)
+
         pts1, pts2, end_size = bottom_perspective_coordinates(img, percent)
         M = cv2.getPerspectiveTransform(pts1, pts2)
         dst = cv2.warpPerspective(img, M, end_size)
-        plt.subplot(121), plt.imshow(img), plt.title('Input')
-        plt.subplot(122), plt.imshow(dst), plt.title('Output')
-        plt.show()
+        cv2.imwrite(join(PATH_TO_TARGET_DIRECTORY,
+                         PATH_TO_IMAGE.split("/")[-1].replace(".JPG", "_BOTTOM_" + str(percent) + ".JPG")), dst)
+
+
+if __name__ == "__main__":
+    img = cv2.imread(PATH_TO_IMAGE)
+    write_to_output(img)
