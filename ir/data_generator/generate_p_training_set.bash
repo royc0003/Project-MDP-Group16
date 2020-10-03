b0659dbcf0dc85142bc98d6fcceb52dc81fdbb80
ongcj <chengjie13@hotmail.com>
2020-10-03 19:16:46 +0800

    re captured and reprocessed x

--- ir/data_generator/generate_p_training_set.bash
+++ ir/data_generator/generate_p_training_set.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # first random sample 100 from processed data
-python random_sample.py 50 /Users/ongcj/Dev/mdp/ir/data/processed/w_resized /Users/ongcj/Dev/mdp/ir/data/training/w/p
+python random_sample.py 50 /Users/ongcj/Dev/mdp/ir/data/processed/x_resized /Users/ongcj/Dev/mdp/ir/data/training/x/p
 # out of the random 100 samples, random rotate 30 images
-python random_rotate.py 50 /Users/ongcj/Dev/mdp/ir/data/training/w/p /Users/ongcj/Dev/mdp/ir/data/training/w/p
+python random_rotate.py 50 /Users/ongcj/Dev/mdp/ir/data/training/x/p /Users/ongcj/Dev/mdp/ir/data/training/x/p
 # random resize everything
-python random_resize.py 100 /Users/ongcj/Dev/mdp/ir/data/training/w/p /Users/ongcj/Dev/mdp/ir/data/training/w/p
+python random_resize.py 100 /Users/ongcj/Dev/mdp/ir/data/training/x/p /Users/ongcj/Dev/mdp/ir/data/training/x/p
 # generate left right top bottom perspective
-python generate_perspective.py /Users/ongcj/Dev/mdp/ir/data/processed/w_resized/IMG_1075_RESIZED.JPG /Users/ongcj/Dev/mdp/ir/data/training/w/p
+python generate_perspective.py /Users/ongcj/Dev/mdp/ir/data/processed/x_resized/IMG_1132_RESIZED.JPG /Users/ongcj/Dev/mdp/ir/data/training/x/p
 
