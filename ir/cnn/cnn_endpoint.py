ad30dd856e606f1b7d12e7c877af5c8f5db706e1
ongcj <chengjie13@hotmail.com>
2020-10-10 23:31:10 +0800

    fix endpoint at rpi. communiate with cnn model is also done

--- ir/cnn/cnn_endpoint.py
+++ ir/cnn/cnn_endpoint.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
 import pickle
+import numpy as np
 from model.cnn import CNN
+import cv2
 
 cnn = CNN("gray")
 app = Flask(__name__)
@@ -10,17 +12,19 @@ api = Api(app)
 
 class Predict(Resource):
     def post(self):
-        content = request.data
-        img = pickle.loads(content)  # get serialized data
+        data = request.data
+        img = cv2.imdecode(np.fromstring(data, dtype=np.uint8), cv2.IMREAD_COLOR)
+        # img = pickle.loads(content)
+        print(img)
         gray, results = cnn.raw_predict(img)
         json_results = []
         for result in results:
             json_result = {
                 'image': result[0],
-                'position': results[1]
+                'position': result[1]
             }
             json_results.append(json_result)
-        return jsonify(json_results), 201
+        return make_response(jsonify(json_results), 201)
 
 
 api.add_resource(Predict, '/predict/')
--- ir/laptop/laptop_end_test.py
+++ ir/laptop/laptop_end_test.py
@@ -1,41 +1,33 @@
 from flask import Flask
 from flask_restful import Api, Resource
+import requests
 import cv2
-import codecs, json
-import numpy as np
 import pickle
 
 app = Flask(__name__)
 api = Api(app)
-
-# left_cascade = cv2.CascadeClassifier('../models/left_cascade.xml')
-# up_cascade = cv2.CascadeClassifier('../models/up_cascade.xml')
-# right_cascade = cv2.CascadeClassifier('../models/right_cascade.xml')
-# down_cascade = cv2.CascadeClassifier('../models/down_cascade.xml')
-
 cam = cv2.VideoCapture(0)
+cnn_url = 'http://127.0.0.1:5000/predict/'
 
 
 def takePic():
     ret, img = cam.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    file = open('data.pkl', 'wb')
-
-    pickle.dump(gray, file)
-    file.close()
-
-    return file
+    img_encoded = cv2.imencode(".jpg", img)[1]
+    return img_encoded
 
 
 class ImgReg(Resource):
 
     def get(self):
+        # this returns a pickle img
         data = takePic()
-        print(data)
-        return {"data": str(data)}
+        headers = {"Content-type": "text/plain"}
+        x = requests.post(cnn_url, data=data.tostring(), headers=headers)
+        print(x.text)
+        return {"result": x.text}
 
 
 api.add_resource(ImgReg, "/ImgReg")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
