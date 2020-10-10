01f162aea69990abfc2901ab6421dd8a78824fc3
ongcj <chengjie13@hotmail.com>
2020-10-10 22:26:56 +0800

    set up cnn endpoint to receive and predict image from rpi

--- ir/cnn/README.md
+++ ir/cnn/README.md
@@ -1,4 +1,6 @@
 `laptop_cnn.py` run this to use laptop webcam to detect trained objects.
 
+`python 3.6.1`
+
 To-Do: 
 try out colored training data. color seems to be a very strong features. 
--- ir/cnn/cnn_endpoint.py
+++ ir/cnn/cnn_endpoint.py
@@ -1,14 +1,28 @@
-from flask import FLask, request
-
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
 import pickle
-from .model.cnn import CNN
+from model.cnn import CNN
 
 cnn = CNN("gray")
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route('/', methods=['POST'])
-def receiveImage():
-    content = request.data
-    img = pickle.loads(content)  # get serialized data
-      # return a no content response
+class Predict(Resource):
+    def post(self):
+        content = request.data
+        img = pickle.loads(content)  # get serialized data
+        gray, results = cnn.raw_predict(img)
+        json_results = []
+        for result in results:
+            json_result = {
+                'image': result[0],
+                'position': results[1]
+            }
+            json_results.append(json_result)
+        return jsonify(json_results), 201
+
+
+api.add_resource(Predict, '/predict/')
+if __name__ == '__main__':
+    app.run(debug=True)
